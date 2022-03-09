c0a8b7c8a1a314488ac8602e87c6ab9963d1d800
rjswn97 <81522323+bakgom123@users.noreply.github.com>
2022-03-09 00:48:37 -0500

    Add files via upload

---
+++ labs/07/script.ada
@@ -0,0 +1,67 @@
+Script started on 2022-02-22 23:53:37-05:00 [TERM="xterm-256color" TTY="/dev/pts/0" COLUMNS="271" LINES="12"]
+]0;gy24@remotel3: ~/cs214/labs/07[01;32mgy24@remotel3[00m:[01;34m~/cs214/labs/07[00m$ cat average.adb
+-- average.adb "test-drives" function Average.
+-- Precondition: theArray is an array of numbers.
+-- Output: the average of the numbers.
+--
+-- Begun by: Dr. Adams, CS 214 at Calvin College.
+-- Completed by: David Yoo (gy24)
+-- Date: Feb 22, 2022
+-----------------------------------------------
+
+with Ada.Text_IO; use Ada.Text_IO;             -- Put(String)
+with Ada.Float_Text_IO; use Ada.Float_Text_IO; -- Put(Float)
+
+procedure average is
+
+-- Declare Vector type
+type Vector is array (Positive range<>) of Float;
+
+-- Define array0 as an array containing no values
+array0 : Vector := (0.0, 0.0);
+-- Define array1 as an array containing 9, 8, 7, 6
+array1 : Vector := (9.0, 8.0, 7.0, 6.0);
+----------------------------------------------
+-- sum() sums the values in an array           -
+-- Receive: anArray, an array of numbers     -
+-- Return: the sum of the values in anArray. -
+----------------------------------------------
+
+-- Replace this line with definition of sum()
+ function sum(A: Vector) return Float is 
+    Total : Float := 0.0; 
+  begin 
+    for I in A'Range 
+    loop 
+      Total := Total + A(I); 
+    end loop; 
+    return Total; 
+  end sum;
+
+-- Replace this line with documentation and definition of average()
+ function average(B: Vector) return Float is  
+   begin 
+    if B'length = 0 then
+      return 0.0;
+    else
+      return sum(B) / Float(B'length);
+    end if;
+   end average;
+
+begin
+   Put(" average 0 is ");
+   Put( average(array0) );
+   New_line;
+   Put(" average 1 is ");
+   Put( average(array1) );
+   New_line;
+end average;
+
+]0;gy24@remotel3: ~/cs214/labs/07[01;32mgy24@remotel3[00m:[01;34m~/cs214/labs/07[00m$ gnatmake aveage[K[K[Krage
+gnatmake: "average" up to date.
+]0;gy24@remotel3: ~/cs214/labs/07[01;32mgy24@remotel3[00m:[01;34m~/cs214/labs/07[00m$ ./avear[K[Krage
+ average 0 is  0.00000E+00
+ average 1 is  7.50000E+00
+]0;gy24@remotel3: ~/cs214/labs/07[01;32mgy24@remotel3[00m:[01;34m~/cs214/labs/07[00m$ exit
+
+Script done on 2022-02-22 23:54:21-05:00 [COMMAND_EXIT_CODE="0"]
---
+++ labs/07/script.java
@@ -0,0 +1,72 @@
+Script started on 2022-02-22 23:44:21-05:00 [TERM="xterm-256color" TTY="/dev/pts/0" COLUMNS="271" LINES="12"]
+]0;gy24@remotel3: ~/cs214/labs/07[01;32mgy24@remotel3[00m:[01;34m~/cs214/labs/07[00m$ cat Average.java
+/* Average.java is a driver for method avg().
+ *
+ * Started by: Prof. Adams, for CS 214 at Calvin College
+ *
+ * Student Name: David Yoo(gy24)
+ * Date: Feb 22, 2022
+ *
+ * Precondition: theArray, is an array of double values.
+ * Output: the average of the numbers in theArray.
+ ***************************************************************/
+
+import java.io.*;
+
+public class Average
+{
+  public static void main(String[] args) {
+
+      // Replace this line with definitions of array0 and array1...
+      {
+        double[] array0 = new double[0];
+        double[] array1 = {9.0, 8.0, 7.0, 6.0};
+
+      // output the average of the values in the arrays
+     System.out.println("The first average is " + avg(array0));
+     System.out.println("The second average is " + avg(array1));
+      }
+  }
+  
+
+  /* sum() is a method that computes the sum of all the values
+   * in an array of doubles.
+   * Receive: anArray, an array of doubles.
+   * Return: the sum of the values in anArray.
+   ************************************************************/
+
+   // Replace this line with the definition of sum()... 
+  public static double sum(double[] theArray){ 
+    double total = 0.0;
+    for (int i = 0; i < theArray.length; i++){
+        total += theArray[i];
+    }
+    return total;
+}
+
+  
+
+  /* avg() is a method that computes the average of the values
+   * in an array.
+   * Receive: anArray, an array of doubles.
+   * Return: the average of the values in anArray.
+   **************************************************************/
+  
+   // Replace this line with the definition of avg()...
+  public static double avg(double anArray[])
+   {
+     if (anArray == null || anArray.length <= 0)
+        return 0.0;
+     else
+        return sum(anArray) / anArray.length;
+   }
+
+}
+
+]0;gy24@remotel3: ~/cs214/labs/07[01;32mgy24@remotel3[00m:[01;34m~/cs214/labs/07[00m$ javac -deprecation Average.java
+]0;gy24@remotel3: ~/cs214/labs/07[01;32mgy24@remotel3[00m:[01;34m~/cs214/labs/07[00m$ java Average
+The first average is 0.0
+The second average is 7.5
+]0;gy24@remotel3: ~/cs214/labs/07[01;32mgy24@remotel3[00m:[01;34m~/cs214/labs/07[00m$ exit
+
+Script done on 2022-02-22 23:44:45-05:00 [COMMAND_EXIT_CODE="0"]
